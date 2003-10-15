186378d4b5f5d62ac6d4a6e3f1c5cc265a03088b
Bryan Drewery <bryan@shatow.net>
2003-10-15 17:31:16 +0000

    Updates
    svn: 56

--- scripts/irssi/wraith.pl
+++ scripts/irssi/wraith.pl
@@ -5,7 +5,7 @@ use Irssi 20020121.2020 ();
 $VERSION = "1.01";
 %IRSSI = (
           authors     => 'bryan P38',
-          contact     => 'bryan\@shatow.net, bryan on EFnet',
+          contact     => 'bryan@shatow.net, bryan on EFnet',
           name        => 'wraith-auth',
           description => 'private auth script for botpack wraith',
           license     => 'ALL?',
@@ -38,7 +38,7 @@ sub auth($) {
     $hash = $words[0];
   }
 
-  Irssi::print "Authing: $hash";
+#  Irssi::print "Authing: $hash";
 
   $secpass = Irssi::settings_get_str('auth_secpass');
   $authkey = Irssi::settings_get_str('auth_authkey');
@@ -95,9 +95,10 @@ Irssi::signal_add "dcc chat message", sub {
   
 };
 
-Irssi::settings_add_str('auth', 'auth_password', '');
+#Irssi::settings_add_str('auth', 'auth_password', '');
 Irssi::settings_add_str('auth', 'auth_secpass', '');
 Irssi::settings_add_str('auth', 'auth_authkey', '');
 
 Irssi::command_bind("auth", "cmd_auth");
 
+Irssi::print "Wraith authorization script by bryan loaded. \$Rev: \$";
