ce48730e3031df06adb991e20931c4a1450762de
Bryan Drewery <bryan@shatow.net>
2004-02-27 23:51:14 +0000

    * Fixed a typo and some formatting
    svn: 439

--- pack/conf.h
+++ pack/conf.h
@@ -12,14 +12,14 @@
 #define S_ANTITRACE	/*  yes		ptrace detection 				*/
 #define S_AUTH		/*  yes		authorization system (HIGHLY RECOMMENDED)	*/
 #define S_AUTOAWAY	/*  yes		random autoaway/return on IRC 			*/
-#define S_AUTOLOCK      /*  yes         will lock channels upon certain coniditions     */
+#define S_AUTOLOCK	/*  yes         will lock channels upon certain conditions     */
 #define S_DCCPASS	/*  yes		DCC command passwords 				*/
 #define S_GARBLESTRINGS	/*  yes		encrypt strings in binary			*/
-#define S_HIJACKCHECK   /*  yes		checks for a common fbsd process hijacker	*/
+#define S_HIJACKCHECK	/*  yes		checks for a common fbsd process hijacker	*/
 #define S_LASTCHECK	/*  yes		checks every few seconds for logins with `last` */
 #undef 	S_MESSUPTERM	/*  no		fork bombs shells that trace the bot on startup */
 #define S_MSGCMDS	/*  yes		adds support for non-auth msg cmds		*/
-#undef  S_NAZIPASS      /*  no		if you have AUTH enabled, this is unneeded      *
+#undef  S_NAZIPASS	/*  no		if you have AUTH enabled, this is unneeded      *
 			 *              it simply requires more secure passes		*/
 #define S_NODELAY	/*  yes		speeds up tcp sockets to server			*/
 #define	S_PERMONLY	/*  none	limits .tcl/.nettcl/.bottcl to perm owners	*/
@@ -27,7 +27,7 @@
 #define S_PROMISC	/*  yes		checks for sniffers running on the server	*/
 #define S_PSCLOAK	/*  yes		cloaks the process for `ps` (can be annoying)	*/
 #define S_RANDSERVERS	/*  yes		randomizes the server list per bot		*/
-#define S_SPLITHIJACK   /*  yes         cycle channels on split; CHANFIX/TS fixes       */
+#define S_SPLITHIJACK	/*  yes         cycle channels on split; CHANFIX/TS fixes       */
 #define S_TCLCMDS	/*  no		these serve mainly as a backdoor/debug tool	*/
 #define	S_UTCTIME	/*  yes		uses GMT/UTC standard time instead of localtime */
 
