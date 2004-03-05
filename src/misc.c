3f91f1e2c2956ed4b082bc9245841b98fe47286e
Bryan Drewery <bryan@shatow.net>
2004-03-05 17:10:58 +0000

    svn: 491

--- src/misc.c
+++ src/misc.c
@@ -473,7 +473,7 @@ void show_banner(int idx)
 {
   dumplots(-dcc[idx].sock, "", wbanner()); /* we use sock so that colors aren't applied to banner */
   dprintf(idx, "\n \n");
-  dprintf(idx, STR("info, bugs, suggestions, comments:\n- http://www.shatow.net/wraith/ -\n"));
+  dprintf(idx, STR("info, bugs, suggestions, comments:\n- http://www.shatow.net/wraith/ -\n \n"));
 }
 
 /* show motd to dcc chatter */
