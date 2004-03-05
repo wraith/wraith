1c81e11201c73cb1c1a4fc9a9d076914080d2e96
Bryan Drewery <bryan@shatow.net>
2004-03-05 23:02:04 +0000

    * Removed some useless code
    svn: 494

--- src/main.c
+++ src/main.c
@@ -382,12 +382,6 @@ static void got_term(int z)
   putlog(LOG_MISC, "*", STR("RECEIVED TERMINATE SIGNAL (IGNORING)"));
 }
 
-static void got_stop(int z) 
-{
-  putlog(LOG_MISC, "*", STR("GOT SIGSTOP POSSIBLE HIJACK."));
-  exit(1);
-}
-
 static void got_abort(int z)
 {
 #ifdef DEBUG_CONTEXT
@@ -1466,8 +1460,6 @@ int main(int argc, char **argv)
   sigaction(SIGFPE, &sv, NULL);
   sv.sa_handler = got_term;
   sigaction(SIGTERM, &sv, NULL);
-  sv.sa_handler = got_stop;
-  sigaction(SIGSTOP, &sv, NULL);
 #ifdef S_HIJACKCHECK
   sv.sa_handler = got_cont;
   sigaction(SIGCONT, &sv, NULL);
