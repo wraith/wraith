fc3ab45fde35259856bf9023bb16357cf807085e
Bryan Drewery <bryan@shatow.net>
2004-03-09 22:20:37 +0000

    * Fixed some bugs that valgrind found (mainly overlapping strcpys)
    svn: 497

--- src/main.c
+++ src/main.c
@@ -1923,7 +1923,8 @@ Context;
 
     /* Free unused structures. */
     garbage_collect();
-
+    
+    buf[0] = 0;
     xx = sockgets(buf, &i); 
     if (xx >= 0) {		/* Non-error */
       int idx;
