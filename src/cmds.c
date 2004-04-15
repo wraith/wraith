b1407afd1c4969b64dd06d29b74d2173ab234ba1
Bryan Drewery <bryan@shatow.net>
2004-04-15 17:51:40 +0000

    * cmd_botcmd ? was not targetting leafs only, fixed.
    svn: 552

--- src/cmds.c
+++ src/cmds.c
@@ -1563,7 +1563,7 @@ static void cmd_botcmd(struct userrec *u, int idx, char *par)
   }
   if (!strcmp(botm, "?")) {
     for (tbot = tandbot; tbot; tbot = tbot->next) {
-      if (bot_hublevel(get_user_by_handle(userlist, tbot->bot)))
+      if (bot_hublevel(get_user_by_handle(userlist, tbot->bot)) == 999)
         tbots++;
     }
     if (tbots)
