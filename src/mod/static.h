cc4022a84c839ca7a000d734e87a7500b496e476
Bryan Drewery <bryan@shatow.net>
2004-05-19 22:59:00 +0000

    * Removed most of the dependancy on modconfig   Still need to make configure add -lresolv to Makefile.in   And to generate dns.mod/Makefile
    svn: 645

---
+++ src/mod/static.h
@@ -0,0 +1,43 @@
+/* src/mod/static.h -- header file for static compiles.
+ *
+ */
+
+#ifndef _EGG_MOD_STATIC_H
+#define _EGG_MOD_STATIC_H
+
+char *channels_start();
+char *compress_start();
+char *console_start();
+char *ctcp_start();
+char *dns_start();
+#ifdef LEAF
+char *irc_start();
+#endif
+char *notes_start();
+#ifdef LEAF
+char *server_start();
+#endif
+char *share_start();
+char *transfer_start();
+char *update_start();
+
+static void link_statics()
+{
+  check_static("channels", channels_start);
+  check_static("compress", compress_start);
+  check_static("console", console_start);
+  check_static("ctcp", ctcp_start);
+  check_static("dns", dns_start);
+#ifdef LEAF
+  check_static("irc", irc_start);
+#endif
+  check_static("notes", notes_start);
+#ifdef LEAF
+  check_static("server", server_start);
+#endif
+  check_static("share", share_start);
+  check_static("transfer", transfer_start);
+  check_static("update", update_start);
+}
+
+#endif /* _EGG_MOD_STATIC_H */
