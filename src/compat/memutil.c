cedc10bf5ecd71949d12277ff9cd176148d3e6c7
Bryan Drewery <bryan@shatow.net>
2004-05-25 17:54:30 +0000

    * #undef strdup   #undef str_redup
      Now using our own compat functions for strdup/str_redup
    
    
    svn: 681

--- src/compat/memutil.c
+++ src/compat/memutil.c
@@ -16,3 +16,11 @@ void str_redup(char **str, const char *newstr)
         egg_memcpy(*str, newstr, len);
 }
 
+char *strdup(const char *entry)
+{
+  char *target = (char*)malloc(strlen(entry) + 1);
+  if (target == 0) return 0;
+  strcpy(target, entry);
+  return target;
+}
+
--- src/compat/memutil.h
+++ src/compat/memutil.h
@@ -1,8 +1,10 @@
 #ifndef _MEMUTIL_H
 #define _MEMUTIL_H
 
+#undef strdup
+#undef str_redup
 
 void str_redup(char **, const char *);
-
+char *strdup(const char *);
 
 #endif /* !_MEMUTIL_H */
