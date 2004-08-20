8715561f374d01a126098b439923b648ae1a3aab
Bryan Drewery <bryan@shatow.net>
2004-08-20 16:20:14 +0000

    * Removed define RANDSERVERS
    svn: 1154

--- src/cfg.c
+++ src/cfg.c
@@ -473,9 +473,7 @@ static void servers_changed(struct cfg_entry * entry, char * olddata, int * vali
     clearq(serverlist);
     serverlist = NULL;
   }
-#ifdef S_RANDSERVERS
   shuffle(slist, ",");
-#endif /* S_RANDSERVERS */
   p = strdup(slist);
   add_server(p);
   free(p);
