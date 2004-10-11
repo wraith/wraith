306f86556cd33d02eef4d4cb0de76e9258d8349c
Bryan Drewery <bryan@shatow.net>
2004-10-11 19:16:26 +0000

    * Changed CREDITS slightly
    svn: 1266

--- src/cmds.c
+++ src/cmds.c
@@ -556,9 +556,7 @@ static void cmd_about(struct userrec *u, int idx, char *par)
   dprintf(idx, STR(" * $bpasswd$b for beta testing, and his dedication to finding bugs.\n"));
   dprintf(idx, STR(" * $bextort$b for finding misc bugs.\n"));
   dprintf(idx, STR(" * $bpgpkeys$b for finding bugs, and providing input.\n"));
-  dprintf(idx, STR(" * $bqFox$b for providing an mIRC $md5() alias, not requiring a dll or >6.03\n"));
-  dprintf(idx, STR(" * $bSith_Lord$b helping test ipv6 on the bot (admin@elitepackets.com)\n"));
-  dprintf(idx, STR(" * $bExcelsior$b for finding a bug on BSD with the ipv6, and for celdrop which inspired many features.\n"));
+  dprintf(idx, STR(" * $bExcelsior$b for celdrop which inspired many features.\n"));
   dprintf(idx, STR(" * $bsyt$b for giving me inspiration to code a more secure bot.\n"));
   dprintf(idx, STR(" * $bBlackjac$b for helping with the bx auth script with his Sentinel script.\n"));
   dprintf(idx, STR(" * $bMystikal$b for various bugs\n"));
