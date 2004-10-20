a1fb5f04703731f3f395432c2a2fed37499a3823
Bryan Drewery <bryan@shatow.net>
2004-10-20 04:07:17 +0000

    * Wrapped some more memory functions
    svn: 1303

--- src/compat/memutil.c
+++ src/compat/memutil.c
@@ -1,6 +1,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include "memcpy.h"
+#include "src/main.h"
+
+extern void * __real_malloc(size_t);
+extern void * __real_realloc(void *, size_t);
+
+void *
+__wrap_malloc(size_t size)
+{
+  void *x = NULL;
+
+  x = (void *) __real_malloc(size);
+  if (x == NULL)
+    fatal("Memory allocation failed", 0);
+
+  return x;
+}
+
+void *
+__wrap_realloc(void *ptr, size_t size)
+{
+  void *x = NULL;
+
+  if (ptr == NULL)
+    return malloc(size);
+
+  x = (void *) __real_realloc(ptr, size);
+  if (x == NULL && size > 0)
+    fatal("Memory re-allocation failed", 0);
+
+  return x;
+}
+
+void *
+__wrap_calloc(size_t nmemb, size_t size)
+{
+  void *x = NULL;
+
+  x = (void *) malloc(nmemb * size);
+  if (x)
+    egg_bzero(x, nmemb * size);
+
+  return x;
+}
 
 void 
 __wrap_str_redup(char **str, const char *newstr)
--- src/compat/memutil.h
+++ src/compat/memutil.h
@@ -1,9 +1,15 @@
 #ifndef _MEMUTIL_H
 #define _MEMUTIL_H
 
+#undef malloc
+#undef realloc
+#undef calloc
 #undef str_redup
 #undef strdup
 
+void *malloc(size_t);
+void *realloc(void *, size_t);
+void *calloc(size_t, size_t);
 void str_redup(char **, const char *);
 char *strdup(const char *);
 
