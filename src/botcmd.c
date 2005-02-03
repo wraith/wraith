e02a7146d016e60a3bdf518da66af00508e72fab
Bryan Drewery <bryan@shatow.net>
2005-02-03 23:03:32 +0000

    svn: 1676

--- src/botcmd.c
+++ src/botcmd.c
@@ -1214,7 +1214,7 @@ static void bot_rsim(char *botnick, char *code, char *msg)
   int ridx = -1, idx = -1, i = 0, rconmask;
   unsigned long status = 0;
   char *nick = NULL, *cmd = NULL, *rconchan = NULL, buf[UHOSTMAX] = "", *par = NULL, *parp = NULL;
-  struct userrec *u = get_user_by_handle(botnick);
+  struct userrec *u = get_user_by_handle(userlist, botnick);
 
   if (bot_hublevel(u) == 999) {
     putlog(LOG_WARN, "*", "BOTCMD received from a leaf. HIJACK.");
