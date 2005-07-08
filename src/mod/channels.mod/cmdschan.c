6172ce6c3764a4f20a43d8d461fdd7f6dc948d0e
Bryan Drewery <bryan@shatow.net>
2005-07-08 23:00:47 +0000

    * Fixed 'chanset *' not working.
    svn: 2216

--- src/mod/channels.mod/cmdschan.c
+++ src/mod/channels.mod/cmdschan.c
@@ -1306,7 +1306,7 @@ static void cmd_chanset(int idx, char *par)
     }
   }
 
-  putlog(LOG_CMDS, "*", "#%s# chanset (%s) %s", dcc[idx].nick, chan->dname, par);
+  putlog(LOG_CMDS, "*", "#%s# chanset (%s) %s", dcc[idx].nick, all ? "*" : chan->dname, par);
   
   if (do_chanset(result, all ? NULL : chan, par, DO_LOCAL | DO_NET) == ERROR) {
     dprintf(idx, "Error trying to set { %s } on %s: %s\n", par, all ? "all channels" : chan->dname, result);
