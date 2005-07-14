af97e3d5aa000737792e8035547dad6dbbe5fe74
Bryan Drewery <bryan@shatow.net>
2005-07-14 15:57:40 +0000

    * Fixed bot username casing causing bad cookies.
    svn: 2255

--- src/mod/irc.mod/irc.c
+++ src/mod/irc.mod/irc.c
@@ -282,7 +282,7 @@ makecookie(char *chn, char *bnick)
   if (strlen(chname) > 2)
     chname[3] = 0;
   strtoupper(chname);
-
+  strtolower(bnick);
   simple_sprintf(tohash, "%c%s%s%s%s%c", settings.salt2[0], bnick, chname, &ts[4], randstring, settings.salt2[15]);
   hash = MD5(tohash);
   buf = (char *) my_calloc(1, 20);
@@ -312,6 +312,7 @@ checkcookie(char *chn, char *bnick, char *cookie)
     chname[3] = 0;
   strtoupper(chname);
   /* hash!rand@ts */
+  strtolower(bnick);
   simple_sprintf(tohash, "%c%s%s%s%s%c", settings.salt2[0], bnick, chname, &ts[4], randstring, settings.salt2[15]);
   hash = MD5(tohash);
   if (!(hash[8] == cookie[0] && hash[16] == cookie[1] && hash[18] == cookie[2]))
