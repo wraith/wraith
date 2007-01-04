942526bd592390d5d6e7d58933cd263bb4b41ac6
Bryan Drewery <bryan@shatow.net>
2007-01-04 04:16:07 +0000

    * 1.2.12 release
    svn: 3119

--- src/main.c
+++ src/main.c
@@ -70,7 +70,7 @@ extern int		optind;
 
 const time_t 	buildts = BUILDTS;		/* build timestamp (UTC) */
 const char	*revision = REVISION;
-const char	*egg_version = "1.2.12-devel";
+const char	*egg_version = "1.2.12";
 
 bool	used_B = 0;		/* did we get started with -B? */
 int 	role;
