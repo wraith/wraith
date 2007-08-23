1c3680523886308c14e66a13ff5ac70ca3e4e08d
Bryan Drewery <bryan@shatow.net>
2007-08-23 01:07:19 +0000

    * Fix getdate/rev for svk
    svn: 3534

--- misc/getdate.sh
+++ misc/getdate.sh
@@ -1,3 +1,16 @@
 #! /bin/sh
-svn info | grep "Last Changed Date" | sed "s/^.*: \(.* \)[-+].*$/\1/"
+parse_date() {
+  echo "$1" | grep "Last Changed Date" | sed "s/Last Changed Date: \(.* \)[-+].*$/\1/"
+}
+
+info=$(svn info 2> /dev/null)
+if [ 0 -eq $? ]; then
+  date=$(parse_date "$info")
+else
+  url=$(svk info|grep "Mirrored From"|sed -e 's/Mirrored From: \(.*\),.*/\1/')
+
+  info=$(svn info $url)
+  date=$(parse_date "$info")
+fi
+echo $date
 
