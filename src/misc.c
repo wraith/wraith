abc867258e13f0c23f5e56680bec6f7aeecc7d6d
Bryan Drewery <bryan@shatow.net>
2008-05-27 18:13:12 +0000

    * Fix spacing error in ad
    svn: 3777

--- src/misc.c
+++ src/misc.c
@@ -354,7 +354,7 @@ void show_banner(int idx)
   dprintf(-dcc[idx].sock,     " -------------------------------------------------------- \n");
   dprintf(-dcc[idx].sock, STR("|             - http://wraith.botpack.net/ -             |\n"));
   dprintf(-dcc[idx].sock, STR("|  Get Shell/Irc/Web hosting @ http://www.xzibition.com  |\n"));
-  dprintf(-dcc[idx].sock, STR("|  Use coupon code 'wraith' for 30%% off lifetime        |\n"));
+  dprintf(-dcc[idx].sock, STR("|  Use coupon code 'wraith' for 30%% off lifetime         |\n"));
   dprintf(-dcc[idx].sock,     " -------------------------------------------------------- \n");
   dprintf(idx, " \n");
 
