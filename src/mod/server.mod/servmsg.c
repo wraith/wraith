716923d492c88d46655c8cc08b933383efd40f9a
Bryan Drewery <bryan@shatow.net>
2009-05-10 09:48:11 -0700

    * Cache 005 into DP_CACHE

--- src/mod/server.mod/servmsg.c
+++ src/mod/server.mod/servmsg.c
@@ -240,6 +240,11 @@ got005(char *from, char *msg)
 {
   char *tmp = NULL, *p, *p2;
 
+  //Cache the results for later parsing if needed (after a restart)
+  //Sending 'VERSION' does not work on all servers, plus this speeds
+  //up a restart by removing the need to wait for the information
+  dprintf(DP_CACHE, ":%s 500 %s", from, msg);
+
   newsplit(&msg); /* nick */
 
   while ((tmp = newsplit(&msg))[0]) {
