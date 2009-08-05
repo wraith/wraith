5cf79810da5a3295a7778be5072090ca0a9c3006
Bryan Drewery <bryan@shatow.net>
2009-08-05 09:26:04 -0500

    * Disable obscure shell errors

--- src/shell.c
+++ src/shell.c
@@ -726,9 +726,13 @@ void werr(int errnum)
   }
   */
   /*  printf("\n[%lu]+  Stopped                 %s\r\n", job, basename(binname)); */
+#ifdef OBSCURE_ERRORS
   sdprintf(STR("Error %d: %s"), errnum, werr_tostr(errnum));
   printf(STR("*** Error code %d\n\n"), errnum);
   printf(STR("Segmentation fault\n"));
+#else
+  printf(STR("Error %d: %s\n"), errnum, werr_tostr(errnum));
+#endif
   fatal("", 0);
   exit(0);				//gcc is stupid :)
 }
