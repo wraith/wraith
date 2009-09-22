44b3fc70332a3b2f60c1e7e6e3064b62df67b2b1
Bryan Drewery <bryan@shatow.net>
2009-09-21 21:13:01 -0500

    * Add more comments from RFC1035

--- src/adns.h
+++ src/adns.h
@@ -5,11 +5,30 @@
 #ifndef _ADNS_H_
 #define _ADNS_H_
 
+/* RFC1035
+TYPE            value and meaning
+A               1 a host address
+NS              2 an authoritative name server
+MD              3 a mail destination (Obsolete - use MX)
+MF              4 a mail forwarder (Obsolete - use MX)
+CNAME           5 the canonical name for an alias
+SOA             6 marks the start of a zone of authority
+MB              7 a mailbox domain name (EXPERIMENTAL)
+MG              8 a mail group member (EXPERIMENTAL)
+MR              9 a mail rename domain name (EXPERIMENTAL)
+NULL            10 a null RR (EXPERIMENTAL)
+WKS             11 a well known service description
+PTR             12 a domain name pointer
+HINFO           13 host information
+MINFO           14 mailbox or mail list information
+MX              15 mail exchange
+TXT             16 text strings
+*/
 
 #define DNS_A		1
-#define DNS_AAAA	28
-#define DNS_PTR		12
 #define DNS_CNAME	5
+#define DNS_PTR		12
+#define DNS_AAAA	28
 
 
 #define DNS_IPV4	1
