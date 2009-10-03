e54d552ab466bf8be04c7b710fedb47e6ddfce7a
Bryan Drewery <bryan@shatow.net>
2009-10-02 20:11:55 -0500

    * Add auto encrypt/decrypt/migration of files
    This will encode the encryption type into the first few bytes of the file.
    This properly handles opening the old socksfile format, as well as
    the old userfile format.

--- src/EncryptedStream.h
+++ src/EncryptedStream.h
@@ -7,10 +7,24 @@ namespace bd {
 #include <iostream>
 #include <bdlib/src/Stream.h>
 #include <bdlib/src/String.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define ENC_AES_256_ECB 	1
+#define ENC_AES_256_CBC		2
+#define ENC_BASE64_BROKEN	4
+#define ENC_BASE64		8
+#define ENC_KEEP_NEWLINES	16
+#define ENC_NO_HEADER		32
+
+#define ENC_DEFAULT 		(ENC_AES_256_ECB)
 
 class EncryptedStream : public bd::Stream {
   private:
         bd::String key;
+        void apply_filters(bd::String& buf, const char enc_flags) const;
+        void unapply_filters(bd::String& buf, const char enc_flags) const;
 
   protected:
 
@@ -19,7 +33,11 @@ class EncryptedStream : public bd::Stream {
         EncryptedStream(bd::String& keyStr) : Stream(), key(keyStr) {};
         EncryptedStream(EncryptedStream& stream) : Stream(stream), key(stream.key) {};
 
-        virtual bd::String gets(size_t, char delim = 0);
-        virtual void puts (const bd::String& string);
+        virtual int loadFile(const int fd);
+        virtual int loadFile(const char* fname) { return bd::Stream::loadFile(fname); }
+
+        virtual int writeFile(const int fd, const char enc_flags) const;
+        virtual int writeFile(const int fd) const { return writeFile(fd, ENC_DEFAULT); }
+        int writeFile(const char* fname, mode_t mode = (S_IRUSR|S_IWUSR)) const { return bd::Stream::writeFile(fname, mode); }
 };
 #endif
