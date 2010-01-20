b74b94517fdaed2effa86d6002a95b1f501b11e3
Bryan Drewery <bryan@shatow.net>
2010-01-20 17:40:03 -0600

    * Speedup mop/mdop kicks

--- src/mod/irc.mod/mode.c
+++ src/mod/irc.mod/mode.c
@@ -1092,7 +1092,7 @@ gotmode(char *from, char *msg)
               if (role <= 2)
                 tputs(serv, tmp, len);
               else
-                dprintf(DP_SERVER, "%s", tmp);
+                dprintf(DP_MODE, "%s", tmp);
             } else {
               if (u) {
                 simple_snprintf(tmp, sizeof(tmp), "Mass deop on %s by %s", chan->dname, m->nick);
@@ -1111,7 +1111,7 @@ gotmode(char *from, char *msg)
                   if (role <= 2)
                     tputs(serv, tmp, len);
                   else
-                    dprintf(DP_SERVER, "%s", tmp);
+                    dprintf(DP_MODE, "%s", tmp);
                 } else { 
                   if (u) {
                     simple_snprintf(tmp, sizeof(tmp), "Mass op on %s by %s", chan->dname, m->nick);
