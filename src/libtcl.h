3209392364c16632a36d5890e219c8f0fd6ed2fe
Bryan Drewery <bryan@shatow.net>
2011-12-27 23:40:53 -0600

    * No longer need to define these headers

--- src/libtcl.h
+++ src/libtcl.h
@@ -12,13 +12,6 @@
 
 #include ".defs/libtcl_post.h"
 
-typedef int (*Tcl_Eval_t)(Tcl_Interp*, const char*);
-typedef Tcl_Command (*Tcl_CreateCommand_t)(Tcl_Interp*, const char*, Tcl_CmdProc*, ClientData, Tcl_CmdDeleteProc*);
-typedef const char* (*Tcl_GetStringResult_t)(Tcl_Interp*);
-typedef void (*Tcl_DeleteInterp_t)(Tcl_Interp*);
-typedef Tcl_Interp* (*Tcl_CreateInterp_t)(void);
-typedef void (*Tcl_FindExecutable_t)(const char*);
-typedef int (*Tcl_Init_t)(Tcl_Interp*);
 
 #define STDVAR (ClientData cd, Tcl_Interp *interp, int argc, const char *argv[])
 
