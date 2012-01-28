945c5156a819c62a6bd6d458434463696ad8c99b
Bryan Drewery <bryan@shatow.net>
2012-01-28 03:11:20 -0600

    * Pass false to daysdur()

--- src/chanprog.c
+++ src/chanprog.c
@@ -259,7 +259,7 @@ void tell_verbose_uptime(int idx)
 # endif
 #endif /* HAVE_GETRUSAGE */
 
-  daysdur(now, online_since, s, sizeof(s));
+  daysdur(now, online_since, s, sizeof(s), false);
 
   if (backgrd)
     strlcpy(s1, "background", sizeof(s1));
@@ -271,7 +271,7 @@ void tell_verbose_uptime(int idx)
   }
   simple_snprintf(outbuf, sizeof(outbuf), "Online for %s", s);
   if (restart_time) {
-    daysdur(now, restart_time, s, sizeof(s));
+    daysdur(now, restart_time, s, sizeof(s), false);
     size_t olen = strlen(outbuf);
     simple_snprintf(&outbuf[olen], sizeof(outbuf) - olen, " (%s %s ago)", restart_was_update ? "updated" : "restarted", s);
   }
