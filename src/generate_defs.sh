dbad518a61b42ef4b7c0d2e26cdd00eaf7f5f707
Bryan Drewery <bryan@shatow.net>
2012-02-19 12:14:10 -0600

    * Speedup symbol generating by executing generate_symbol.sh less often

--- src/generate_defs.sh
+++ src/generate_defs.sh
@@ -48,14 +48,16 @@ for file in $(grep -l DLSYM_GLOBAL src/*.c|grep -v "src/_"); do
 
     if [ "${typedef%;}" = "${typedef}" ]; then
       echo "Error: Unable to generate typedef for: ${symbol}" >&2
-      echo "$typedef"
+      echo "$typedef" >&2
       rm -rf $TMPFILE
       exit 1
     fi
 
     #pipe typedef into generate_symbol.sh
-    echo "$typedef" | src/generate_symbol.sh >> $defsFile_wrappers 2>> $defsFile_post
-  done
+    if [ -n "$typedef" ]; then
+      echo "$typedef"
+    fi
+  done | src/generate_symbol.sh >> $defsFile_wrappers 2>> $defsFile_post
 
   echo "}" >> $defsFile_wrappers
   echo "}" >> $defsFile_post
