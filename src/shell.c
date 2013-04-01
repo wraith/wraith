8b0e5193b272339e18af3fadaf02d698e73f7d68
Bryan Drewery <bryan@shatow.net>
2013-04-01 18:55:08 -0500

    Wraith requires misc/compat8x now; 7x is EOL

--- src/shell.c
+++ src/shell.c
@@ -654,7 +654,7 @@ const char *werr_tostr(int errnum)
   case ERR_TOOMANYBOTS:
     return STR("Too many bots defined. 5 max. Too many will lead to klines.\nSpread out into multiple accounts/shells/ip ranges.");
   case ERR_LIBS:
-    return STR("Failed to load required libraries.\nEnsure that 32bit compat libs are installed, and openssl.\nLinux: ia32-libs\nFreeBSD: misc/compat7x");
+    return STR("Failed to load required libraries.\nEnsure that 32bit compat libs are installed, and openssl.\nLinux: ia32-libs\nFreeBSD: misc/compat8x");
   default:
     return STR("Unforseen error");
   }
