b31840c06460bb506adb279d175f471599d67772
Bryan Drewery <bryan@shatow.net>
2014-02-06 19:12:40 -0600

    Add .cpp rules

--- build/build.mk
+++ build/build.mk
@@ -1,5 +1,25 @@
 depcomp = /bin/sh $(top_srcdir)/build/autotools/depcomp
 
+%.o: %.cpp $(top_srcdir)/src/stringfix
+	@echo -e "[CC]	\033[1m$*\033[0m"
+	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
+	  if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) $(CPPFLAGS) -E $< | $(top_srcdir)/src/stringfix > $<.ii; then \
+	    mv '.deps/$*.TPo' '.deps/$*.Po'; \
+	  else \
+	    rm -f '.deps/$*.Tpo'; \
+	    exit 1; \
+	  fi; \
+	else \
+	  libtool=no source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
+	  $(CXX) $(CXXFLAGS) $(CPPFLAGS) -E $< | $(top_srcdir)/src/stringfix > $<.ii; \
+	fi; \
+	if ! $(CXX) $(CXXFLAGS) $(CPPFLAGS) -c $<.ii -o $@; then \
+	  rm -f $<.ii; \
+	  exit 1; \
+	else \
+	  rm -f $<.ii; \
+	fi
+
 %.o: %.c $(top_srcdir)/src/stringfix
 	@echo -e "[C ]	\033[1m$*\033[0m"
 	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
