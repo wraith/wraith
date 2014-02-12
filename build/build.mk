7472a59cada76f96fd849c1de5c8b45f081f1358
Bryan Drewery <bryan@shatow.net>
2014-02-12 06:44:38 -0600

    Name stringfixed objects as .So so non-stringfix objects don't get transformed

--- build/build.mk
+++ build/build.mk
@@ -1,10 +1,10 @@
 depcomp = /bin/sh $(top_srcdir)/build/autotools/depcomp
 
 .SUFFIXES:
-.SUFFIXES: .c .cc .h .o
+.SUFFIXES: .c .cc .h .o .So
 
-.cc.o:
-	@echo -e "[CC]	\033[1m$*\033[0m"
+.cc.So:
+	@echo -e "{CC}	\033[1m$*\033[0m"
 	trap "rm -f '.deps/$*.Tpo' $*.ii; exit 1" 1 2 3 5 10 13 15; \
 	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
 	  if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) $(CPPFLAGS) -E $< | $(top_srcdir)/src/stringfix > $*.ii; then \
@@ -18,8 +18,8 @@ depcomp = /bin/sh $(top_srcdir)/build/autotools/depcomp
 	$(CXX) $(CXXFLAGS) $(CPPFLAGS) -c $*.ii -o $@; \
 	rm -f '.deps/$*.Tpo' $*.ii
 
-.c.o:
-	@echo -e "[C ]	\033[1m$*\033[0m"
+.c.So:
+	@echo -e "{C }	\033[1m$*\033[0m"
 	trap "rm -f '.deps/$*.Tpo' $*.i; exit 1" 1 2 3 5 10 13 15; \
 	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
 	  if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) $(CPPFLAGS) -E $< | $(top_srcdir)/src/stringfix > $*.i; then \
@@ -32,3 +32,29 @@ depcomp = /bin/sh $(top_srcdir)/build/autotools/depcomp
 	fi; \
 	$(CXX) $(CXXFLAGS) $(CPPFLAGS) -c $*.i -o $@; \
 	rm -f '.deps/$*.Tpo' $*.i
+
+.cc.o:
+	@echo -e "[CC]	\033[1m$*\033[0m"
+	trap "rm -f '.deps/$*.Tpo'; exit 1" 1 2 3 5 10 13 15; \
+	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
+	  if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) $(CPPFLAGS) -c $< -o $@; then \
+	    mv '.deps/$*.TPo' '.deps/$*.Po'; \
+	  fi; \
+	else \
+	  libtool=no source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
+	  $(CXX) $(CXXFLAGS) $(CPPFLAGS) -c $< -o $@; \
+	fi; \
+	rm -f '.deps/$*.Tpo'
+
+.c.o:
+	@echo -e "[C]	\033[1m$*\033[0m"
+	trap "rm -f '.deps/$*.Tpo'; exit 1" 1 2 3 5 10 13 15; \
+	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
+	  if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) $(CPPFLAGS) -c $< -o $@; then \
+	    mv '.deps/$*.TPo' '.deps/$*.Po'; \
+	  fi; \
+	else \
+	  libtool=no source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
+	  $(CXX) $(CXXFLAGS) $(CPPFLAGS) -c $< -o $@; \
+	fi; \
+	rm -f '.deps/$*.Tpo'
