wraith/wraith

commit c8664e1986fa113679fd92add76fbc69dda4f07f

Fix help not being garbled.
Broken since e7332578 and 3c226c5d.

src/Makefile.in
@@ -93,9 +93,9 @@ responses.cc: $(RESPONSESFILE) $(top_srcdir)/build/makeres.sh
 
 cmds.cc: help.h
 
-help.h: $(HELPFILE) $(top_srcdir)/build/makehelp.sh
+help.h: $(HELPFILE) $(top_srcdir)/build/makehelp.sh stringfix
 	@echo -e "[H]	\033[1m$*\033[0m"
-	@$(top_srcdir)/build/makehelp.sh < $(HELPFILE) > $@
+	@$(top_srcdir)/build/makehelp.sh < $(HELPFILE) | ./stringfix > $@
 
 sorthelp: sorthelp.cc ../lib/bdlib/src/libbdlib.a
 	@echo -e "Compiling: \033[1msorthelp\033[0m"

src/stringfix.cc
@@ -13,12 +13,12 @@
 #include "config.h"
 #endif
 
-#define WTF 1524
+#define MAX_LINE_LEN 16384
 int help = 0;
 
 void garble(char **inptr, char **outptr)
 {
-  char *in = *inptr, *out = NULL, *p = NULL, obuf[WTF] = "";
+  char *in = *inptr, *out = NULL, *p = NULL, obuf[MAX_LINE_LEN] = "";
   size_t chars = 0;
   unsigned char x = 0;
 
@@ -98,7 +98,7 @@ void garble(char **inptr, char **outptr)
 
 void processline(char *line)
 {
-  char tmpin[WTF] = "", tmpout[WTF] = "", *in = NULL, *out = NULL;
+  char tmpin[MAX_LINE_LEN] = "", tmpout[MAX_LINE_LEN] = "", *in = NULL, *out = NULL;
   size_t outlen = 0;
 
   strcpy(tmpin, line); 

commit e76d79d7149bbd1ceb4600370494dc6590605e97

Fix garbled help broken since c8664e198

build/makehelp.sh
@@ -29,9 +29,9 @@ sed -e 's,["],\\&,g' | while read -r line; do
 		esac
 
 		if [ ${garble} -eq 0 ]; then
-			printf "  {%d, \"%s\", %d, \"" ${type_int} "${cmd}" ${garble}
+			printf "  {%d, \"%s\", 0, \"" ${type_int} "${cmd}"
 		else
-			printf "  {%d, \"%s\", %d, STR(\"" ${type_int} "${cmd}" ${garble}
+			printf "  {%d, \"%s\", STR(\"" ${type_int} "${cmd}"
 	fi
 		needcomma=0
 	else

src/Makefile.in
@@ -108,7 +108,7 @@ cmds.cc: help.h
 
 help.h: $(HELPFILE) $(top_srcdir)/build/makehelp.sh $(STRINGFIX)
 	@echo -e "{GEN}	\033[1m$<\033[0m -> \033[1m$@\033[0m"
-	$(top_srcdir)/build/makehelp.sh < $(HELPFILE) | $(STRINGFIX) > $@
+	$(top_srcdir)/build/makehelp.sh < $(HELPFILE) | $(STRINGFIX) help > $@
 
 sorthelp: sorthelp.cc ../lib/bdlib/src/libbdlib.a
 	@echo -e "[CXX]	\033[1m$<\033[0m"

src/cmds.cc
@@ -681,8 +681,8 @@ static void cmd_help(int idx, char *par)
         dprintf(idx, "Showing you help for '%s' (%s):\n", match, flg);
         help_t *h_entry = NULL;
         if ((h_entry = findhelp(match)) != NULL) {
-          if (h_entry->garble)
-            showhelp(idx, &fr, degarble(h_entry->garble, h_entry->desc));
+          if (h_entry->garble_len)
+            showhelp(idx, &fr, degarble(h_entry->garble_len, h_entry->desc));
           else
             showhelp(idx, &fr, h_entry->desc);
         }

src/cmds.h
@@ -16,7 +16,7 @@ typedef struct {
 typedef struct {
   int type;
   const char *cmd;
-  int garble;
+  size_t garble_len;
   const char *desc;
 } help_t;
 