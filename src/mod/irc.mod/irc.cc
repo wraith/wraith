76084d1fafc6da8968177a291072b65244a3005e
Bryan Drewery <bryan@shatow.net>
2016-02-11 18:53:38 -0800

    Clear out cached member pointers that are not used.

--- src/mod/irc.mod/irc.cc
+++ src/mod/irc.mod/irc.cc
@@ -1170,6 +1170,9 @@ killmember(struct chanset_t *chan, char *nick, bool cacheMember)
 
   if (cacheMember) {
     x->last = now;
+    x->user = NULL;
+    x->next = NULL;
+    x->tried_getuser = 0;
     // Don't delete here, will delete when it expires from the cache.
     (*chan->channel.cached_members)[x->userhost] = x;
   } else {
