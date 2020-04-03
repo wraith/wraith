wraith/wraith

commit 97dcff1c3504f00731cd0499011d694b0bcc49d9

Fix -Wwritable-strings warnings

src/mod/channels.mod/cmdschan.cc
@@ -108,10 +108,13 @@ usage:
 	}
     }
   }
+  const char *reason = NULL;
   if (!par[0])
-    par = "requested";
-  else if (strlen(par) > MASKREASON_MAX)
+    reason = "requested";
+  else if (strlen(par) > MASKREASON_MAX) {
     par[MASKREASON_MAX] = 0;
+    reason = par;
+  }
   if (strlen(who) > UHOSTMAX - 4)
     who[UHOSTMAX - 4] = 0;
   /* Fix missing ! or @ BEFORE checking against myself */
@@ -139,15 +142,15 @@ usage:
     s[70] = 0;
   }
   if (chan) {
-    u_addmask(type, chan, s, dcc[idx].nick, par, expire_time ? now + expire_time : 0, 0);
-    if (par[0] == '*') {
+    u_addmask(type, chan, s, dcc[idx].nick, reason, expire_time ? now + expire_time : 0, 0);
+    if (reason[0] == '*') {
       sticky = 1;
-      par++;
-      putlog(LOG_CMDS, "*", "#%s# (%s) +%s %s %s (%s) (sticky)", dcc[idx].nick, dcc[idx].u.chat->con_chan, cmd, s, chan->dname, par);
-      dprintf(idx, "New %s sticky %s: %s (%s)\n", chan->dname, cmd, s, par);
+      reason++;
+      putlog(LOG_CMDS, "*", "#%s# (%s) +%s %s %s (%s) (sticky)", dcc[idx].nick, dcc[idx].u.chat->con_chan, cmd, s, chan->dname, reason);
+      dprintf(idx, "New %s sticky %s: %s (%s)\n", chan->dname, cmd, s, reason);
     } else {
-      putlog(LOG_CMDS, "*", "#%s# (%s) +%s %s %s (%s)", dcc[idx].nick, dcc[idx].u.chat->con_chan, cmd, s, chan->dname, par);
-      dprintf(idx, "New %s %s: %s (%s)\n", chan->dname, cmd, s, par);
+      putlog(LOG_CMDS, "*", "#%s# (%s) +%s %s %s (%s)", dcc[idx].nick, dcc[idx].u.chat->con_chan, cmd, s, chan->dname, reason);
+      dprintf(idx, "New %s %s: %s (%s)\n", chan->dname, cmd, s, reason);
     }
     if (!conf.bot->hub) {
       if (type == 'e' || type == 'I')
@@ -159,15 +162,15 @@ usage:
     } else
       write_userfile(idx);
   } else {
-    u_addmask(type, NULL, s, dcc[idx].nick, par, expire_time ? now + expire_time : 0, 0);
-    if (par[0] == '*') {
+    u_addmask(type, NULL, s, dcc[idx].nick, reason, expire_time ? now + expire_time : 0, 0);
+    if (reason[0] == '*') {
       sticky = 1;
-      par++;
-      putlog(LOG_CMDS, "*", "#%s# (GLOBAL) +%s %s (%s) (sticky)", dcc[idx].nick, cmd, s, par);
-      dprintf(idx, "New sticky %s: %s (%s)\n", cmd, s, par);
+      reason++;
+      putlog(LOG_CMDS, "*", "#%s# (GLOBAL) +%s %s (%s) (sticky)", dcc[idx].nick, cmd, s, reason);
+      dprintf(idx, "New sticky %s: %s (%s)\n", cmd, s, reason);
     } else {
-      putlog(LOG_CMDS, "*", "#%s# (GLOBAL) +%s %s (%s)", dcc[idx].nick, cmd, s, par);
-      dprintf(idx, "New %s: %s (%s)\n", cmd, s, par);
+      putlog(LOG_CMDS, "*", "#%s# (GLOBAL) +%s %s (%s)", dcc[idx].nick, cmd, s, reason);
+      dprintf(idx, "New %s: %s (%s)\n", cmd, s, reason);
     }
     if (!conf.bot->hub) {
       for (chan = chanset; chan != NULL; chan = chan->next) {
@@ -344,10 +347,10 @@ static void cmd_masks(const char type, int idx, char *par)
 
   if (!strcasecmp(par, "all")) {
     putlog(LOG_CMDS, "*", "#%s# %ss all", dcc[idx].nick, str_type);
-    tell_masks(type, idx, 1, "");
+    tell_masks(type, idx, 1, NULL);
   } else if (!strcasecmp(par, "global")) {
     putlog(LOG_CMDS, "*", "#%s# %ss global", dcc[idx].nick, str_type);
-    tell_masks(type, idx, 1, "", 1);
+    tell_masks(type, idx, 1, NULL, 1);
   } else {
     putlog(LOG_CMDS, "*", "#%s# %ss %s", dcc[idx].nick, str_type, par);
     tell_masks(type, idx, 0, par);
@@ -681,7 +684,8 @@ static void cmd_stick_yn(int idx, char *par, int yn)
 {
   int i = 0, j;
   struct chanset_t *chan = NULL;
-  char *stick_type = NULL, s[UHOSTLEN] = "", chname[81] = "", type = 0, *str_type = NULL;
+  char s[UHOSTLEN] = "", chname[81] = "", type = 0;
+  const char *stick_type = NULL, *str_type = NULL;
   maskrec *channel_list = NULL;
 
   stick_type = newsplit(&par);

src/adns.cc
@@ -165,8 +165,8 @@ const char *dns_ip = NULL;
 
 static int make_header(char *buf, int id);
 static int cut_host(const char *host, char *query);
-static int read_resolv(char *fname);
-static void read_hosts(char *fname);
+static int read_resolv(const char *fname);
+static void read_hosts(const char *fname);
 static int get_dns_idx();
 //static void dns_resend_queries();
 static int cache_find(const char *);
@@ -840,7 +840,7 @@ static const char *dns_next_server()
 	return (servers[cur_server].ip);
 }
 
-static void add_dns_server(char *ip)
+static void add_dns_server(const char *ip)
 {
 	servers = (dns_server_t *) realloc(servers, (nservers+1) *
 	    sizeof(*servers));
@@ -849,7 +849,7 @@ static void add_dns_server(char *ip)
 	sdprintf("Added NS: %s", ip);
 }
 
-static void add_host(char *host, char *ip)
+static void add_host(const char *host, const char *ip)
 {
 	hosts = (dns_host_t *) realloc(hosts, (nhosts+1) * sizeof(*hosts));
 	hosts[nhosts].host = strdup(host);
@@ -927,7 +927,7 @@ static int read_thing(char *buf, char *ip)
 	return (skip + len);
 }
 
-static int read_resolv(char *fname)
+static int read_resolv(const char *fname)
 {
 	FILE *fp;
 	char buf[512], ip[512];
@@ -948,7 +948,7 @@ static int read_resolv(char *fname)
 	return count;
 }
 
-static void read_hosts(char *fname)
+static void read_hosts(const char *fname)
 {
 	FILE *fp;
 	char buf[512], ip[512], host[512];

src/binary.cc
@@ -214,7 +214,9 @@ bin_checksum(const char *fname, int todo)
     munmap(map, size);
     close(fd);
 
-    return ".";
+    hash[0] = '.';
+    hash[1] = '\0';
+    return hash;
   } else if (todo & WRITE_CHECKSUM) {
     bd::AtomicFile* newbin = new bd::AtomicFile();
 
@@ -802,7 +804,8 @@ void conf_to_bin(conf_t *in, bool move, int die)
                            bot->net.ip6 ? bot->net.ip6 : "");
     }
 
-  simple_snprintf(settings.conf_hubs, sizeof(settings.conf_hubs), in->hubs.join(',').c_str());
+  strlcpy(settings.conf_hubs, in->hubs.join(',').c_str(),
+      sizeof(settings.conf_hubs));
 
   newbin = binname;
 //  tellconfig(&settings); 

src/binds.cc
@@ -52,7 +52,7 @@ static int bind_vcheck_hits (bind_table_t *table, const char *match, struct flag
 static void bind_table_really_del(bind_table_t *table);
 static void bind_entry_really_del(bind_table_t *table, bind_entry_t *entry);
 
-static char *maze = ".===============================================================.\n|  ,-----------------,                                          |\n| /| HELP THE BUNNY  |==========.===.   .===.   .===========.   |\n|| |    FIND HIS     |          |   |   |   |   |      .-.  | E |\n|| |  EASTER EGGS!   |  |===|   |   |   |   |   |..==./xxx\\ | N |\n|| |_________________|          |   |       |   /<<<<<\\    || D |\n||/_________________/   .======='   |   .   |   \\>>>>>/xxxx/--. |\n|   |   |           |   |           |   |   |   |`'==''---; * *`\\\n|   |   '==========='   |   |======='   |   |   |   ,===. \\* * */\n|   |                   |               |   |   |   |   |  '--'`|\n|   '===============|   '===.   |===.   |   |   |==='   '=======|\n|                           |       |   |   |   |               |\n|   |===============.   .   '===|   |   |===|   |   .=======.   |\n|                   |   |           |   |   |   |   |       |   |\n|   .===========.   |   |   |===.   |   |   |   |   |   |   |   |\n|   |           |   |   |       |   |   |   |   |   |   |   |   |\n|   |   .===.   |   |   |===.   '===|   |   '==='   |   |   |   |\n|   |   |   |   |   |   |   |       |   |           |   |   |   |\n|   '==='   /`\\ '==='   |   '===.   |   '===========|   |   |   |\n|          / : |                |   |               |   |   |   |\n| _.._=====| '/ '===|   .======='   '===========.   |   |   |   |\n  .-._ '-\"` (=======.   |   .===============.   |   |   '===.   |\n_/  |/   e  e\\==.   |   |   |               |   |   |       |   |\n| S ||  >   @ )<|   |   |   |   .=======.   |   |   |===.   |   |\n| T | \\  '--`/  |   |   |   |   |       |   |   |   |   |   |   |\n| A | / '--<`   |   |   |   |   |   |   |   |   '==='   |   '   |\n| R || ,    \\\\  |           |   |   |   |   |           |       |\n| T |; ;     \\\\__'======.   |   |   '==='   |   .===.   |   |   |\n|   / /      |.__)==,   |   |   |           |   |   |   |   |   |\n|  (_/,--.   ; //\"\"\"\\\\  |   |   '==========='   |   '==='   |   |\n|  { `|   \\_/  ||___||  |   |                   |           |   |\n|   ;-\\   / |  |(___)|  |   '===========.   |   '=======.   |   |\n|   |  | /  |  |XXXXX|  |               |   |           |   |   |\n|   | /  \\  '-,\\XXXXX/  |   .==========='   '=======.   |   |   |\n|   | \\__|----' `\"\"\"`   |   |                       |   |   |   |\n|   '==================='   '======================='   '==='   |\n|                                                               |\n'==============================================================='";
+static const char *maze = ".===============================================================.\n|  ,-----------------,                                          |\n| /| HELP THE BUNNY  |==========.===.   .===.   .===========.   |\n|| |    FIND HIS     |          |   |   |   |   |      .-.  | E |\n|| |  EASTER EGGS!   |  |===|   |   |   |   |   |..==./xxx\\ | N |\n|| |_________________|          |   |       |   /<<<<<\\    || D |\n||/_________________/   .======='   |   .   |   \\>>>>>/xxxx/--. |\n|   |   |           |   |           |   |   |   |`'==''---; * *`\\\n|   |   '==========='   |   |======='   |   |   |   ,===. \\* * */\n|   |                   |               |   |   |   |   |  '--'`|\n|   '===============|   '===.   |===.   |   |   |==='   '=======|\n|                           |       |   |   |   |               |\n|   |===============.   .   '===|   |   |===|   |   .=======.   |\n|                   |   |           |   |   |   |   |       |   |\n|   .===========.   |   |   |===.   |   |   |   |   |   |   |   |\n|   |           |   |   |       |   |   |   |   |   |   |   |   |\n|   |   .===.   |   |   |===.   '===|   |   '==='   |   |   |   |\n|   |   |   |   |   |   |   |       |   |           |   |   |   |\n|   '==='   /`\\ '==='   |   '===.   |   '===========|   |   |   |\n|          / : |                |   |               |   |   |   |\n| _.._=====| '/ '===|   .======='   '===========.   |   |   |   |\n  .-._ '-\"` (=======.   |   .===============.   |   |   '===.   |\n_/  |/   e  e\\==.   |   |   |               |   |   |       |   |\n| S ||  >   @ )<|   |   |   |   .=======.   |   |   |===.   |   |\n| T | \\  '--`/  |   |   |   |   |       |   |   |   |   |   |   |\n| A | / '--<`   |   |   |   |   |   |   |   |   '==='   |   '   |\n| R || ,    \\\\  |           |   |   |   |   |           |       |\n| T |; ;     \\\\__'======.   |   |   '==='   |   .===.   |   |   |\n|   / /      |.__)==,   |   |   |           |   |   |   |   |   |\n|  (_/,--.   ; //\"\"\"\\\\  |   |   '==========='   |   '==='   |   |\n|  { `|   \\_/  ||___||  |   |                   |           |   |\n|   ;-\\   / |  |(___)|  |   '===========.   |   '=======.   |   |\n|   |  | /  |  |XXXXX|  |               |   |           |   |   |\n|   | /  \\  '-,\\XXXXX/  |   .==========='   '=======.   |   |   |\n|   | \\__|----' `\"\"\"`   |   |                       |   |   |   |\n|   '==================='   '======================='   '==='   |\n|                                                               |\n'==============================================================='";
 
 static int internal_bind_cleanup()
 {

src/botcmd.cc
@@ -51,7 +51,8 @@
 
 static char TBUF[1024] = "";		/* Static buffer for goofy bot stuff */
 
-static void fake_alert(int idx, char *item, char *extra, char *what)
+static void fake_alert(int idx, const char *item, const char *extra,
+    const char *what)
 {
   static time_t lastfake;	/* The last time fake_alert was used */
 
@@ -869,7 +870,8 @@ static void bot_reject(int idx, char *par)
           u = get_user_by_handle(userlist, from);
           if (u) {
             if (!whois_access(u, dcc[idx].user)) {
-              add_note(from, conf.bot->nick, "Sorry, you cannot boot them.", -1, 0);
+              char note[] = "Sorry, you cannot boot them.";
+              add_note(from, conf.bot->nick, note, -1, 0);
               return;
             }
             do_boot(i, from, par);
@@ -1249,13 +1251,16 @@ const botcmd_t *search_botcmd_t(const botcmd_t *table, const char* keyString, si
   return (const botcmd_t*) bsearch(&key, table, elements, sizeof(botcmd_t), comp_botcmd_t);
 }
 
-void parse_botcmd(int idx, const char* code, const char* msg) {
+void parse_botcmd(int idx, const char* code, char* msg) {
   const botcmd_t *cmd = search_botcmd_t((const botcmd_t*)&C_bot, code, lengthof(C_bot) - 1);
 
   if (cmd) {
+    static char blank[] = "";
+    if (msg == NULL)
+      msg = blank;
     /* Found a match */
     if (have_cmd(NULL, cmd->type))
-      (cmd->func) (idx, (char*)msg);
+      (cmd->func) (idx, msg);
   }
 }
 

src/botcmd.h
@@ -5,6 +5,6 @@ void bounce_simul(int, char *);
 void send_remote_simul(int, char *, char *, char *);
 void bot_share(int, char *);
 void init_botcmd(void);
-void parse_botcmd(int, const char*, const char*);
+void parse_botcmd(int, const char*, char*);
 
 #endif /* !_BOTCMD_H */

src/botmsg.cc
@@ -69,7 +69,7 @@ void send_hubs_but(int idx, const char *msg, size_t len)
 
 /* Ditto for tandem bots
  */
-static void send_tand_but(int x, char *buf, size_t len)
+static void send_tand_but(int x, const char *buf, size_t len)
 {
   int i = 0;
 
@@ -80,7 +80,7 @@ static void send_tand_but(int x, char *buf, size_t len)
   }
 }
 
-void botnet_send_cmdpass(int idx, char *cmd, char *pass)
+void botnet_send_cmdpass(int idx, const char *cmd, const char *pass)
 {
   if (tands > 0) {
     char *buf = NULL;
@@ -94,7 +94,9 @@ void botnet_send_cmdpass(int idx, char *cmd, char *pass)
   }
 }
 
-int botnet_send_cmd(char * fbot, char * bot, char *fhnd, int fromidx, char * cmd) {
+int botnet_send_cmd(const char *fbot, const char *bot,
+    const char *fhnd, int fromidx, const char *cmd)
+{
   int i = nextbot(bot);
 
   if (i >= 0) {
@@ -110,7 +112,8 @@ int botnet_send_cmd(char * fbot, char * bot, char *fhnd, int fromidx, char * cmd
   return 0;
 }
 
-void botnet_send_cmd_broad(int idx, char * fbot, char *fhnd, int fromidx, char * cmd) {
+void botnet_send_cmd_broad(int idx, const char *fbot,
+    const char *fhnd, int fromidx, const char *cmd) {
   if (tands > 0) {
     size_t len = simple_snprintf(OBUF, sizeof OBUF, "rc * %s %s %i %s\n", fbot, fhnd, fromidx, cmd);
     send_tand_but(idx, OBUF, len);
@@ -123,7 +126,8 @@ void botnet_send_cmd_broad(int idx, char * fbot, char *fhnd, int fromidx, char *
   }
 }
 
-void botnet_send_cmdreply(char * fbot, char * bot, char * to, char * toidx, char * ln) {
+void botnet_send_cmdreply(const char *fbot, const char *bot, const char *to,
+    const char *toidx, const char *ln) {
   int i = nextbot(bot);
 
   if (i >= 0) {
@@ -144,7 +148,8 @@ void botnet_send_bye(const char *reason)
   }
 }
 
-void botnet_send_chan(int idx, char *botnick, char *user, int chan, char *data)
+void botnet_send_chan(int idx, const char *botnick, const char *user,
+    int chan, const char *data)
 {
   if ((tands > 0) && (chan < GLOBAL_CHANS)) {
     size_t len;
@@ -158,7 +163,8 @@ void botnet_send_chan(int idx, char *botnick, char *user, int chan, char *data)
   }
 }
 
-void botnet_send_act(int idx, char *botnick, char *user, int chan, char *data)
+void botnet_send_act(int idx, const char *botnick, const char *user, int chan,
+    const char *data)
 {
   if ((tands > 0) && (chan < GLOBAL_CHANS)) {
     size_t len;
@@ -192,7 +198,8 @@ void botnet_send_pong(int idx)
   tputs(dcc[idx].sock, "po\n", 3);
 }
 
-void botnet_send_priv (int idx, char *from, char *to, char *tobot, const char *format, ...)
+void botnet_send_priv (int idx, const char *from, const char *to,
+    const char *tobot, const char *format, ...)
 {
   size_t len;
   char tbuf[1024] = "";
@@ -210,28 +217,30 @@ void botnet_send_priv (int idx, char *from, char *to, char *tobot, const char *f
   tputs(dcc[idx].sock, OBUF, len);
 }
 
-void botnet_send_who(int idx, char *from, char *to, int chan)
+void botnet_send_who(int idx, const char *from, const char *to, int chan)
 {
   const size_t len = simple_snprintf2(OBUF, sizeof(OBUF), "w %s %s %D\n", from, to, chan);
 
   tputs(dcc[idx].sock, OBUF, len);
 }
 
-void botnet_send_unlink(int idx, char *who, char *via, char *bot, char *reason)
+void botnet_send_unlink(int idx, const char *who, const char *via,
+    const char *bot, const char *reason)
 {
   const size_t len = simple_snprintf(OBUF, sizeof(OBUF), "ul %s %s %s %s\n", who, via, bot, reason);
 
   tputs(dcc[idx].sock, OBUF, len);
 }
 
-void botnet_send_link(int idx, char *who, char *via, char *bot)
+void botnet_send_link(int idx, const char *who, const char *via,
+    const char *bot)
 {
   const size_t len = simple_snprintf(OBUF, sizeof(OBUF), "l %s %s %s\n", who, via, bot);
 
   tputs(dcc[idx].sock, OBUF, len);
 }
 
-void botnet_send_unlinked(int idx, char *bot, char *args)
+void botnet_send_unlinked(int idx, const char *bot, const char *args)
 {
   if (tands > 0) {
     const size_t len = simple_snprintf(OBUF, sizeof(OBUF), "un %s %s\n", bot, args ? args : "");
@@ -240,7 +249,8 @@ void botnet_send_unlinked(int idx, char *bot, char *args)
   }
 }
 
-void botnet_send_nlinked(int idx, char *bot, char *next, char flag, int vlocalhub, time_t vbuildts, char *vcommit, char *vversion)
+void botnet_send_nlinked(int idx, const char *bot, const char *next, char flag,
+    int vlocalhub, time_t vbuildts, const char *vcommit, const char *vversion)
 {
   if (tands > 0) {
     const size_t len = simple_snprintf(OBUF, sizeof(OBUF), "n %s %s %cD0gc %d %d %s %s\n", bot, next, flag,
@@ -249,14 +259,14 @@ void botnet_send_nlinked(int idx, char *bot, char *next, char flag, int vlocalhu
   }
 }
 
-void botnet_send_traced(int idx, char *bot, char *buf)
+void botnet_send_traced(int idx, const char *bot, const char *buf)
 {
   const size_t len = simple_snprintf(OBUF, sizeof(OBUF), "td %s %s\n", bot, buf);
 
   tputs(dcc[idx].sock, OBUF, len);
 }
 
-void botnet_send_trace(int idx, char *to, char *from, char *buf)
+void botnet_send_trace(int idx, const char *to, const char *from, const char *buf)
 {
   const size_t len = simple_snprintf(OBUF, sizeof(OBUF), "t %s %s %s:%s\n", to, from, buf, conf.bot->nick);
 
@@ -273,7 +283,8 @@ void botnet_send_update(int idx, tand_t * ptr)
   }
 }
 
-void botnet_send_reject(int idx, char *fromp, char *frombot, char *top, char *tobot, char *reason)
+void botnet_send_reject(int idx, const char *fromp, const char *frombot,
+    const char *top, const char *tobot, const char *reason)
 {
   size_t len;
   char to[NOTENAMELEN + 1] = "", from[NOTENAMELEN + 1] = "";
@@ -362,7 +373,8 @@ void botnet_send_var_broad(int idx, variable_t *var) {
   }
 }
 
-void botnet_send_idle(int idx, char *bot, int sock, int idle, char *away)
+void botnet_send_idle(int idx, const char *bot, int sock, int idle,
+    const char *away)
 {
   if (tands > 0) {
     const size_t len = simple_snprintf2(OBUF, sizeof(OBUF), "i %s %D %Ds%s\n", bot, sock, idle, away ? away : "");
@@ -371,7 +383,8 @@ void botnet_send_idle(int idx, char *bot, int sock, int idle, char *away)
   }
 }
 
-void botnet_send_away(int idx, char *bot, int sock, char *msg, int linking)
+void botnet_send_away(int idx, const char *bot, int sock,
+    const char *msg, int linking)
 {
   if (tands > 0) {
     const size_t len = simple_snprintf2(OBUF, sizeof(OBUF), "aw %s%s %D %s\n", ((idx >= 0) && linking) ? "!" : "", bot, sock, msg ? msg : "");
@@ -405,7 +418,7 @@ void botnet_send_join_party(int idx, int linking, int useridx)
   }
 }
 
-void botnet_send_part_idx(int useridx, char *reason)
+void botnet_send_part_idx(int useridx, const char *reason)
 {
   if (tands > 0) {
     const size_t len = simple_snprintf2(OBUF, sizeof(OBUF), "pt %s %s %D %s\n", conf.bot->nick,
@@ -415,7 +428,7 @@ void botnet_send_part_idx(int useridx, char *reason)
   }
 }
 
-void botnet_send_part_party(int idx, int partyidx, char *reason, int silent)
+void botnet_send_part_party(int idx, int partyidx, const char *reason, int silent)
 {
   if (tands > 0) {
     const size_t len = simple_snprintf2(OBUF, sizeof(OBUF), "pt %s%s %s %D %s\n",
@@ -426,7 +439,7 @@ void botnet_send_part_party(int idx, int partyidx, char *reason, int silent)
   }
 }
 
-void botnet_send_nkch(int useridx, char *oldnick)
+void botnet_send_nkch(int useridx, const char *oldnick)
 {
   if (tands > 0) {
     const size_t len = simple_snprintf2(OBUF, sizeof(OBUF), "nc %s %D %s\n", conf.bot->nick, dcc[useridx].sock, dcc[useridx].nick);
@@ -435,7 +448,7 @@ void botnet_send_nkch(int useridx, char *oldnick)
   }
 }
 
-void botnet_send_nkch_part(int butidx, int useridx, char *oldnick)
+void botnet_send_nkch_part(int butidx, int useridx, const char *oldnick)
 {
   if (tands > 0) {
     const size_t len = simple_snprintf2(OBUF, sizeof(OBUF), "nc %s %D %s\n", party[useridx].bot, party[useridx].sock, party[useridx].nick);

src/botnet.cc
@@ -400,7 +400,7 @@ int nextbot(const char *who)
 
 /* Return name of the bot that is directly connected to bot X
  */
-char *lastbot(const char *who)
+const char *lastbot(const char *who)
 {
   tand_t *bot = findbot(who);
 
@@ -836,7 +836,7 @@ void dump_links(int z)
   }
 }
 
-int in_chain(char *who)
+int in_chain(const char *who)
 {
   if (!strcasecmp(who, conf.bot->nick))
     return 1;
@@ -952,7 +952,7 @@ int botunlink(int idx, const char *nick, const char *reason)
          sometimes. While we still search for the bug, provide
          an easy way to clear out those `ghost'-bots.
 				       Fabian (2000-08-02)  */
-      char *ghost = "BUG!!: Found bot `%s' in internal bot list, but it\n"
+      const char *ghost = "BUG!!: Found bot `%s' in internal bot list, but it\n"
 		    "   shouldn't have been there! Removing.\n"
 		    "   This is a known bug we haven't fixed yet. If this\n"
 		    "   bot is the newest eggdrop version available and you\n"
@@ -982,7 +982,7 @@ static void botlink_real(int);
 
 /* Link to another bot
  */
-int botlink(char *linker, int idx, char *nick)
+int botlink(const char *linker, int idx, char *nick)
 {
   struct userrec *u = get_user_by_handle(userlist, nick);
 

commit 9428d58617b74b0fc22befff18046a53fffc5030

Fix +ban crash with reason.
Fallout from 97dcff1c3504.

src/mod/channels.mod/cmdschan.cc
@@ -108,13 +108,11 @@ usage:
 	}
     }
   }
-  const char *reason = NULL;
+  const char *reason = par;
   if (!par[0])
     reason = "requested";
-  else if (strlen(par) > MASKREASON_MAX) {
+  else if (strlen(par) > MASKREASON_MAX)
     par[MASKREASON_MAX] = 0;
-    reason = par;
-  }
   if (strlen(who) > UHOSTMAX - 4)
     who[UHOSTMAX - 4] = 0;
   /* Fix missing ! or @ BEFORE checking against myself */